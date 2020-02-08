ff82f2190e84ce9964185b808af45cc1646fa9cd
writingR <writing0625@gamil.com>
2020-02-08 21:02:54 +0900

    change to the index.js class structure.

--- index.js
+++ index.js
@@ -1,63 +1,109 @@
 // 여기에서 자바스크립트 로직으로 개발하시면 됩니다.
 // class를 사용하면 더욱 좋을 것 같습니다.
 // es6이상의 문법을 사용해주세요.
+// value="테스트입니다."
 
-// enter key로 todo 추가
-let inputBox = document.querySelector("input");
-inputBox.addEventListener("keypress", event => {
+/*
+   2020.02.08
+   class 구조로 변경
+   추가할 기능 : transition(추가,삭제시에) ,item 전체삭제기능(button)
+*/
+
+const addBtn = document.querySelector(".addBtn");
+let inputValue = document.querySelector(".input");
+const container = document.querySelector(".todoList");
+
+class todoListItems {
+  constructor(todoItemValue) {
+    // create the item div
+    this.createContainer(todoItemValue);
+  }
+
+  createContainer(todoItemValue) {
+    // todoInput addBtn Click -> 출력되는 list (type input), button
+    // input(수정시에도 사용)
+    let input = document.createElement("input");
+    input.value = todoItemValue;
+    input.disabled = true;
+    input.classList.add("itemInput");
+    input.classList.add("non-checked");
+    input.type = "text";
+
+    let todoCheck = document.createElement("span");
+    todoCheck.innerHTML = "V";
+    todoCheck.classList.add("itemCheck");
+
+    let itemBox = document.createElement("div");
+    itemBox.classList.add("item");
+
+    // 출력되는 list 안에 completeBtn
+    let completeBtn = document.createElement("button");
+    completeBtn.innerHTML = "완료";
+    completeBtn.classList.add("completeBtn");
+
+    // 출력되는 list 안에 editBtn
+    let editBtn = document.createElement("button");
+    editBtn.innerHTML = "수정";
+    editBtn.classList.add("editBtn");
+
+    // 출력되는 list 안에 removeBtn
+    let removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "삭제";
+    removeBtn.classList.add("removeBtn");
+
+    // todoList class에 div item class 추가
+    container.appendChild(itemBox);
+
+    // div item class에 input, button 추가
+    itemBox.appendChild(todoCheck);
+    itemBox.appendChild(input);
+    itemBox.appendChild(completeBtn);
+    itemBox.appendChild(editBtn);
+    itemBox.appendChild(removeBtn);
+
+    // 일정 체크완료, 체크해제, 수정, 제거 이벤트연결.
+    completeBtn.addEventListener("click", () => this.complete(input));
+    todoCheck.addEventListener("click", () => this.completeRemove(input));
+    editBtn.addEventListener("click", () => this.edit(input));
+    removeBtn.addEventListener("click", () => this.remove(itemBox));
+  }
+
+  // 완료 버튼 클릭시 행해지는 이벤트 함수
+  complete(input) {
+    input.classList = "itemInput checked";
+  }
+
+  completeRemove(input) {
+    input.classList = "itemInput non-checked";
+  }
+
+  // 수정 버튼 클릭시 행해지는 이벤트 함수
+  edit(input) {
+    input.disabled = !input.disabled;
+  }
+
+  // 삭제 버튼 클릭시 행해지는 이벤트 함수
+  remove(todo) {
+    let removeItem = document.querySelector(".item");
+    removeItem.remove(todo);
+  }
+}
+
+let addItem = () => {
+  if (inputValue.value !== "") {
+    new todoListItems(inputValue.value);
+    inputValue.value = "";
+  } else {
+    alert("텍스트를 입력하고 일정을 추가해주세요.");
+  }
+};
+
+// todoInput Button Click -> 일정추가를 위한 코드
+addBtn.addEventListener("click", addItem);
+// enter키 입력시에도 list에 추가되도록 설정
+window.addEventListener("keypress", event => {
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.querySelector("#addTodoBtn").click();
+    document.querySelector(".addBtn").click();
   }
 });
-
-// Write text to the inputbox and press the button to add todo -> 2020.02.07
-function addItem() {
-  // input, 새롭게 추가될 'li', inputbox에서 입력한 text
-  let todoInputValue = document.querySelector("input").value;
-  let li = document.createElement("li");
-  let div = document.createElement("div");
-  let inputText = document.createTextNode(todoInputValue);
-
-  //   li.appendChild(inputText);
-  // add todoItem
-  if (todoInputValue === "") {
-    alert("일정추가를 위해 텍스트를 입력해주세요.");
-  } else {
-    document.querySelector("#todoItems").appendChild(li);
-  }
-  document.querySelector("input").value = "";
-
-  // todoItem('li'tag)에 체크박스, 완료, 수정, 삭제 추가하기
-  // 추가 완료 -> 2020.02.07
-  let inputTextBox = document.createElement("span");
-  let complete = document.createElement("span");
-  let edit = document.createElement("span");
-  let remove = document.createElement("span");
-
-  let completeBtnName = document.createTextNode("완료");
-  let editBtnName = document.createTextNode("수정");
-  let removeBtnName = document.createTextNode("삭제");
-  div.className = "todoBtnBox";
-  complete.className = "todoItemComplete";
-  edit.className = "todoItemEdit";
-  remove.className = "todoItemRemove";
-  inputTextBox.className = "todoItemText";
-  complete.appendChild(completeBtnName);
-  edit.appendChild(editBtnName);
-  remove.appendChild(removeBtnName);
-  li.appendChild(div);
-  div.appendChild(inputTextBox);
-  div.appendChild(complete);
-  div.appendChild(edit);
-  div.appendChild(remove);
-  inputTextBox.appendChild(inputText);
-
-  /*
-    완료,수정,삭제 onclick test! -> 2020.02.07
-    이후에 수정기능 추가 할 예정. 
-  */
-  complete.setAttribute("onclick", "alert('완료체크')");
-  edit.setAttribute("onclick", "alert('수정')");
-  remove.setAttribute("onclick", "alert('일정삭제')");
-}
